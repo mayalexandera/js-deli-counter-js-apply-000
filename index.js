8b76545a2f827243a6c62e52adb48c5d74b6e8ff
Maya Blevins <maya.blevins@gmail.com>
2020-04-21 17:47:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber() {
   orderNumber++;
 }
 
-console.log(takeANumber)
+console.log(takeANumber())
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length >= 1) {
     return `Currently serving ${katzDeliLine.shift()}.`
